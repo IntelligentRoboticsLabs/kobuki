f8ba7434e5d98471a402addb345aa11502d36a5a
Juancams <jc.manzanares.serrano@gmail.com>
2024-04-29 17:08:17 +0200

    Bug
    Signed-off-by: Juancams <jc.manzanares.serrano@gmail.com>

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -90,7 +90,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             'use_sim_time': use_sim_time,
-            'rviz': rviz
+            'rviz': rviz,
             'namespace': namespace
         }.items()
     )
